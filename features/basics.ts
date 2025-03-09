7a4f3493beb17d46b36f3e8bbe5ca9eb1deef1ab
elvisonob <elvisonob@yahoo.com>
2025-03-09 08:27:01 +0000

    optional syntax

--- features/basics.ts
+++ features/basics.ts
@@ -1,14 +1,13 @@
 type User = {
   name: string;
   age: number;
-  job: string;
+  job?: string;
   fiber: () => string;
 };
 
 const users: User = {
   name: 'elvis',
   age: 34,
-  job: 'Software developer',
   fiber() {
     console.log('one love');
     return 'one love';
